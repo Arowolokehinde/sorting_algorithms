92f0576719c305f507f21df40c9cf82ce5585a45
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-23 10:02:17 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,27 +6,22 @@
  * @size: The size of the array
  * Return: The arrays sorted
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	int temp, sort;
+	size_t i, j, temp;
 
-	if (!array || size == 0)
-		return;
-
-	do {
-		sort = 1;
-		for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				sort = 0;
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
-
-	} while (sort == 0);
+	}
 }
